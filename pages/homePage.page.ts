54054ce3600325a7a637bc2fb9c85f71482c213a
Jadson Santos <jadsondossantos5@gmail.com>
2025-10-23 19:02:31 -0300

    feat: home page tests

--- pages/homePage.page.ts
+++ pages/homePage.page.ts
@@ -2,39 +2,43 @@ import { type Page, type Locator } from '@playwright/test'
 
 export class HomePage {
   // locators
-  readonly page: Page;
-  readonly dynamicIdLink: Locator;
+  readonly page: Page
+  readonly dynamicIdLink: Locator
 
   constructor(page: Page) {
-    this.page = page;
+    this.page = page
     this.dynamicIdLink = page.getByRole('link', { name: 'Dynamic ID' })
   }
 
   // methods
   async navigateToHomePage() {
-    await this.page.goto('/');
+    await this.page.goto('/')
   }
 
   async goto(url: string) {
-    await this.page.goto(url);
+    await this.page.goto(url)
   }
 
-  async clickOnDynamicIdLink() {
-    await this.dynamicIdLink.waitFor({state: 'attached'})
-    await this.dynamicIdLink.waitFor({state: 'visible'})
+  public async clickOnDynamicLink() {
+    await this.dynamicIdLink.waitFor({ state: 'attached' })
+    await this.dynamicIdLink.waitFor({ state: 'visible' })
     await this.dynamicIdLink.scrollIntoViewIfNeeded()
-    await this.dynamicIdLink.click();
+    await this.dynamicIdLink.click()
   }
 
   async clickOnLink(linkText: string) {
-    await this.page.getByRole('link', { name: linkText }).click();
+    await this.page.getByRole('link', { name: linkText }).click()
   }
 
   async getPageTitle() {
-    return this.page.title();
+    return this.page.title()
   }
 
   async clickBySelector(selector: string) {
-    await this.page.locator(selector).click();
+    await this.page.locator(selector).click()
+  }
+
+  public async isDynamicLinkExisting() {
+    return await this.dynamicIdLink.isVisible()
   }
 }
--- tests/homePageSuite.spec.ts
+++ tests/homePageSuite.spec.ts
@@ -1,25 +1,30 @@
 import { test, expect } from "@playwright/test";
 import { HomePage } from "../pages/homePage.page";
-let homePage: HomePage;
+import { DynamicIdPage } from '../pages/dynamicIdPage.page'
+let homePage: HomePage
+let dynamicIdPage: DynamicIdPage
 
-test.describe("Home Page Suite", async () => {
-  test("Verify Home Page is loaded", async ({ page }) => {
-    homePage = new HomePage(page);
+test.describe('Smoke Test Suite', { tag: '@smoke' }, () => {
+  test.beforeEach(async ({ page }, testInfo) => {
+    testInfo.setTimeout(testInfo.timeout + 60_000 * 2)
+    await page.goto('/', { waitUntil: 'load' })
+    homePage = new HomePage(page)
+    dynamicIdPage = new DynamicIdPage(page)
+  })
 
-    await test.step("Visit Home Page", async () => {
-      await homePage.navigateToHomePage();
-    });
-
-    await test.step('Verify page is loaded', async () => {
-      await expect(page).toHaveTitle(/UI Test Automation Playground/);
-    });
-
-    await test.step("Verify Dynamic ID Element is Existing", async () => {
-      await expect(homePage.dynamicIdLink).toBeVisible({timeout: 5000});
-    });
-
-    await test.step("Click Dynamic ID Element", async () => {
-      await homePage.clickOnDynamicIdLink();
-    });
-  });
-});
+  test('Visit Home Page Verification', { tag: ['@123'] }, async ({ page }) => {
+    await test.step('Verify Dynamic Link is Existing', async () => {
+      const isDynamicLinkExistingResult: Boolean =
+        await homePage.isDynamicLinkExisting()
+      expect(isDynamicLinkExistingResult).toBeTruthy()
+    })
+    await test.step('Click on Dynamic Link', async () => {
+      await homePage.clickOnDynamicLink()
+    })
+    await test.step('Is Dynamic Id Page title present', async () => {
+      const isDynamicIdTitlePresent: Boolean =
+        await dynamicIdPage.isDynamicIdTitlePresent()
+      expect(isDynamicIdTitlePresent).toBeTruthy()
+    })
+  })
+})
